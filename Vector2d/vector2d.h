fe192ee78499b27249a84a378155839c2e1449f7
4xTime <88463120+4xTime@users.noreply.github.com>
2024-03-18 11:03:21 +0100

    vector2d added

---
+++ Vector2d/vector2d.h
@@ -0,0 +1,43 @@
+#pragma once
+#ifndef H_VECTOR2D_H
+#define H_VECTOR2D_H
+#include <vector>
+
+struct Vector2{
+	float x, y;
+	template <typename T>
+	constexpr Vector2(const T x = 0, const T y = 0) noexcept :x(x), y(y) {}
+	
+	constexpr const Vector2& operator-(const Vector2& arg)const noexcept {
+		return Vector2(x - arg.x, y - arg.y);
+	}
+
+	constexpr const Vector2& operator+(const Vector2& arg)const noexcept {
+		return Vector2(x + arg.x, y + arg.y);
+	}	
+	
+	constexpr Vector2 operator/(const float factor) const noexcept {
+		return Vector2{ x / factor, y / factor };
+	}
+
+	constexpr Vector2 operator*(const float factor) const noexcept {
+		return Vector2{ x * factor, y * factor };
+	}
+
+	constexpr bool operator>(const Vector2& other) const noexcept {
+		return x > other.x && y > other.y;
+	}
+
+	constexpr bool operator>=(const Vector2& other) const noexcept {
+		return x >= other.x && y >= other.y;
+	}
+
+	constexpr bool operator<(const Vector2& other) const noexcept {
+		return x < other.x&& y < other.y;
+	}
+
+	constexpr bool operator<=(const Vector2& other) const noexcept {
+		return x <= other.x && y <= other.y;
+	}
+}; 
+#endif
